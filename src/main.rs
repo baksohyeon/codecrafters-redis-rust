57808425a50f7d775231d29fb9bb1e5f8247b353
Dorito <bori12370@gmail.com>
2024-08-17 15:29:00 +0900

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 mod client;
 
+use std::env;
+
 use client::connection::RedisServer;
 
 
@@ -9,6 +11,15 @@ use client::connection::RedisServer;
 async fn main() -> std::io::Result<()> {
     println!("Redis 서버 시작 중...");
 
-    let server = RedisServer::new("127.0.0.1".to_string(), 6379);
+    let args: Vec<String> = env::args().collect();
+    let port = args
+        .get(2)
+        .and_then(|s| s.parse::<u16>().ok())
+        .unwrap_or(6379);
+
+    println!("port: {}", port);
+    println!("args: {:?}", args);
+
+    let server = RedisServer::new("127.0.0.1".to_string(), port);
     server.run().await
 }
