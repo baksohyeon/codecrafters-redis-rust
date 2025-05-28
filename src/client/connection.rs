6b6af5dc2d89f22fb80d877d2ad2d192cfe43a60
Dorito <bori12370@gmail.com>
2025-05-28 21:09:38 +0900

    codecrafters submit [skip ci]

--- src/client/connection.rs
+++ src/client/connection.rs
@@ -7,6 +7,22 @@ use super::cache_store::CacheStore;
 use super::codec::RespCodec;
 use super::model::RespValue;
 
+// Empty RDB file content (hex decoded)
+const EMPTY_RDB_FILE: &[u8] = &[
+    0x52, 0x45, 0x44, 0x49, 0x53, 0x30, 0x30, 0x31, 0x31, 0xfa, 0x09, 0x72, 0x65, 0x64, 0x69, 0x73,
+    0x2d, 0x76, 0x65, 0x72, 0x05, 0x37, 0x2e, 0x32, 0x2e, 0x30, 0xfa, 0x0a, 0x72, 0x65, 0x64, 0x69,
+    0x73, 0x2d, 0x62, 0x69, 0x74, 0x73, 0xc0, 0x40, 0xfa, 0x05, 0x63, 0x74, 0x69, 0x6d, 0x65, 0xc2,
+    0x6d, 0x08, 0xbc, 0x65, 0xfa, 0x08, 0x75, 0x73, 0x65, 0x64, 0x2d, 0x6d, 0x65, 0x6d, 0xc2, 0xb0,
+    0xc4, 0x10, 0x00, 0xfa, 0x08, 0x61, 0x6f, 0x66, 0x2d, 0x62, 0x61, 0x73, 0x65, 0xc0, 0x00, 0xff,
+    0xf0, 0x6e, 0x3b, 0xfe, 0xc0, 0xff, 0x5a, 0xa2
+];
+
+#[derive(Debug)]
+enum CommandResponse {
+    Normal(RespValue),
+    PsyncWithRdb(RespValue), // FULLRESYNC response followed by RDB file
+}
+
 #[derive(Debug, Clone)]
 pub struct ReplicaConfig {
     pub master_host: Option<String>,
@@ -217,9 +233,27 @@ async fn handle_client(stream: TcpStream, data_store: Arc<Mutex<CacheStore>>, re
             Ok(RespValue::Array(commands)) => {
                 println!("handle_client: redis_reader: {:?}\n commands: {:?} \n \n", redis_reader, commands);
                 let response = process_command(commands, &data_store, &replica_config);
-                redis_writer.write_all(&RespCodec::encode(&response))?;
-                redis_writer.flush()?;
-                println!("handle_client: response: {:?} \n \n", response);
+                
+                match response {
+                    CommandResponse::Normal(resp_value) => {
+                        redis_writer.write_all(&RespCodec::encode(&resp_value))?;
+                        redis_writer.flush()?;
+                        println!("handle_client: response: {:?} \n \n", resp_value);
+                    }
+                    CommandResponse::PsyncWithRdb(resp_value) => {
+                        // First send the FULLRESYNC response
+                        redis_writer.write_all(&RespCodec::encode(&resp_value))?;
+                        redis_writer.flush()?;
+                        println!("handle_client: FULLRESYNC response: {:?}", resp_value);
+                        
+                        // Then send the RDB file in the format: $<length>\r\n<binary_contents>
+                        let rdb_header = format!("${}\r\n", EMPTY_RDB_FILE.len());
+                        redis_writer.write_all(rdb_header.as_bytes())?;
+                        redis_writer.write_all(EMPTY_RDB_FILE)?;
+                        redis_writer.flush()?;
+                        println!("handle_client: sent RDB file ({} bytes)", EMPTY_RDB_FILE.len());
+                    }
+                }
             }
             Ok(other) => {
                 eprintln!("handle_client: Unexpected data type: {:?}", other);
@@ -238,9 +272,9 @@ async fn handle_client(stream: TcpStream, data_store: Arc<Mutex<CacheStore>>, re
 
     Ok(())
 }
-fn process_command(commands: Vec<RespValue>, data_store: &Arc<Mutex<CacheStore>>, replica_config: &Option<ReplicaConfig>) -> RespValue {
+fn process_command(commands: Vec<RespValue>, data_store: &Arc<Mutex<CacheStore>>, replica_config: &Option<ReplicaConfig>) -> CommandResponse {
     if commands.is_empty() {
-        return RespValue::Error("ERR no command specified".to_string());
+        return CommandResponse::Normal(RespValue::Error("ERR no command specified".to_string()));
     }
 
 
@@ -249,36 +283,36 @@ fn process_command(commands: Vec<RespValue>, data_store: &Arc<Mutex<CacheStore>>
         RespValue::BinaryBulkString(b) => {
             match String::from_utf8(b.clone()) {
                 Ok(s) => s.to_uppercase(),
-                Err(_) => return RespValue::Error("ERR invalid command: non-UTF8 data".to_string()),
+                Err(_) => return CommandResponse::Normal(RespValue::Error("ERR invalid command: non-UTF8 data".to_string())),
             }
         },
-        _ => return RespValue::Error("ERR invalid command: expected string".to_string()),
+        _ => return CommandResponse::Normal(RespValue::Error("ERR invalid command: expected string".to_string())),
     };
 
     
 
     match command.as_str() {
-        "PING" => RespValue::SimpleString("PONG".to_string()),
+        "PING" => CommandResponse::Normal(RespValue::SimpleString("PONG".to_string())),
         "SET" => {
             if commands.len() < 3 {
-                return RespValue::Error("ERR wrong number of arguments for 'set' command: expected 3".to_string());
+                return CommandResponse::Normal(RespValue::Error("ERR wrong number of arguments for 'set' command: expected 3".to_string()));
             }
             let mut store = data_store.lock().unwrap();
             let key = match &commands[1] {
                 RespValue::BulkString(s) | RespValue::SimpleString(s) => s.clone(),
                 RespValue::BinaryBulkString(b) => match String::from_utf8(b.clone()) {
                     Ok(s) => s,
-                    Err(_) => return RespValue::Error("ERR invalid key: non-UTF8 data".to_string()),
+                    Err(_) => return CommandResponse::Normal(RespValue::Error("ERR invalid key: non-UTF8 data".to_string())),
                 },
-                _ => return RespValue::Error("ERR invalid key: expected string".to_string()),
+                _ => return CommandResponse::Normal(RespValue::Error("ERR invalid key: expected string".to_string())),
             };
             let value = match &commands[2] {
                 RespValue::BulkString(s) | RespValue::SimpleString(s) => s.clone(),
                 RespValue::BinaryBulkString(b) => match String::from_utf8(b.clone()) {
                     Ok(s) => s,
-                    Err(_) => return RespValue::Error("ERR invalid value: non-UTF8 data".to_string()),
+                    Err(_) => return CommandResponse::Normal(RespValue::Error("ERR invalid value: non-UTF8 data".to_string())),
                 },
-                _ => return RespValue::Error("ERR invalid value: expected string".to_string()),
+                _ => return CommandResponse::Normal(RespValue::Error("ERR invalid value: expected string".to_string())),
             };
             
             let expiry = if commands.len() > 3 {
@@ -288,43 +322,43 @@ fn process_command(commands: Vec<RespValue>, data_store: &Arc<Mutex<CacheStore>>
             };
 
             store.set(key, value, expiry);
-            RespValue::SimpleString("OK".to_string())
+            CommandResponse::Normal(RespValue::SimpleString("OK".to_string()))
         }
         "GET" => {
             if commands.len() < 2 {
-                return RespValue::Error("ERR wrong number of arguments for 'get' command: expected 2".to_string());
+                return CommandResponse::Normal(RespValue::Error("ERR wrong number of arguments for 'get' command: expected 2".to_string()));
             }
 
             let key = match &commands[1] {
                 RespValue::BulkString(s) | RespValue::SimpleString(s) => s.clone(),
                 RespValue::BinaryBulkString(b) => match String::from_utf8(b.clone()) {
                     Ok(s) => s,
-                    Err(_) => return RespValue::Error("ERR invalid key: non-UTF8 data".to_string()),
+                    Err(_) => return CommandResponse::Normal(RespValue::Error("ERR invalid key: non-UTF8 data".to_string())),
                 },
-                _ => return RespValue::Error("ERR invalid key: expected string".to_string()),
+                _ => return CommandResponse::Normal(RespValue::Error("ERR invalid key: expected string".to_string())),
             };
             let store = data_store.lock().unwrap();
             println!("process_command: store: {:?}", store);
             match store.get(&key) {
                 Some(value) => {
                     if value.is_empty() {
-                        return RespValue::Null;
+                        return CommandResponse::Normal(RespValue::Null);
                     }
-                    return RespValue::BulkString(value.clone());
+                    return CommandResponse::Normal(RespValue::BulkString(value.clone()));
                 },
-                None => RespValue::Null,
+                None => CommandResponse::Normal(RespValue::Null),
             }
         }
         "ECHO" => {
             if commands.len() < 2 {
-                return RespValue::Error("ERR wrong number of arguments for 'echo' command: expected 2".to_string());
+                return CommandResponse::Normal(RespValue::Error("ERR wrong number of arguments for 'echo' command: expected 2".to_string()));
             }
-            commands[1].clone()
+            CommandResponse::Normal(commands[1].clone())
         }
         "REPLCONF" => {
             // For the purposes of this challenge, we can safely ignore the arguments
             // and just respond with +OK\r\n ("OK" encoded as a RESP Simple String)
-            RespValue::SimpleString("OK".to_string())
+            CommandResponse::Normal(RespValue::SimpleString("OK".to_string()))
         }
         "PSYNC" => {
             // The master responds with +FULLRESYNC <REPL_ID> 0\r\n
@@ -333,7 +367,7 @@ fn process_command(commands: Vec<RespValue>, data_store: &Arc<Mutex<CacheStore>>
             // 0 is the replication offset of the master
             let repl_id = "8371b4fb1155b71f4a04d3e1bc3e18c4a990aeeb";
             let response = format!("FULLRESYNC {} 0", repl_id);
-            RespValue::SimpleString(response)
+            CommandResponse::PsyncWithRdb(RespValue::SimpleString(response))
         }
         "INFO" => {
             // TODO replica info 
@@ -353,9 +387,9 @@ fn process_command(commands: Vec<RespValue>, data_store: &Arc<Mutex<CacheStore>>
                 role.to_string()
             };
             
-            RespValue::BulkString(info_response)
+            CommandResponse::Normal(RespValue::BulkString(info_response))
         }
-        _ => RespValue::Error(format!("ERR unknown command: {}", command)),
+        _ => CommandResponse::Normal(RespValue::Error(format!("ERR unknown command: {}", command))),
     }
 }
 
